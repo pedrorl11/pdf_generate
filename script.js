8b3db2cb0c45a814b44b1b5dffb1cac7e1f1a458
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-08-20 19:30:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const btnGenerate = document.querySelector("#botao");
 
-
 btnGenerate.addEventListener("click", () => {
-    const content = document.querySelector("#content");
-    const nomeInput = document.querySelector("#nome");
-    const nome = nomeInput.value;
-  
-    const options = {
-      filename: `${nome}.pdf`,
-      image: { type: 'jpeg', quality: 1 },
-      html2canvas: {
-        dpi: 300,
-        letterRendering: true,
-        useCORS: true,
-        scale: 6
-      },
-      jsPDF: { unit: "pt", format: [content.offsetWidth, content.offsetHeight], orientation: "portrait" }
-    };
-  
-    if (nome.trim() !== '') {
-      html2pdf().set(options).from(content).save();
-    } else {
-      alert("Por favor, insira um nome antes de gerar o certificado.");
-    }
-  });
+  const content = document.querySelector("#content");
+  const nomeInput = document.querySelector("#nome");
+  const nome = nomeInput.value;
+
+  // Obter as dimensões reais de largura e altura do conteúdo
+  const larg = content.offsetWidth;
+  const alt = content.offsetHeight;
+
+  const options = {
+    filename: `${nome}.pdf`,
+    image: { type: 'jpeg', quality: 1 },
+    html2canvas: {
+      dpi: 300,
+      letterRendering: true,
+      useCORS: true,
+      scale: 6
+    },
+    jsPDF: { unit: "pt", format: [larg, alt], orientation: "portrait" }
+  };
+
+  if (nome.trim() !== '') {
+    html2pdf().set(options).from(content).save();
+  } else {
+    alert("Por favor, insira um nome antes de gerar o certificado.");
+  }
+});
