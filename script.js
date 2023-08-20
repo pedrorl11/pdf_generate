f2a2baa3c4b88ea870096488d2ab42af5a11b515
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-08-20 19:12:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const btnGenerate = document.querySelector("#botao");
 
-btnGenerate.addEventListener("click", () =>{
+
+btnGenerate.addEventListener("click", () => {
     const content = document.querySelector("#content");
     const nomeInput = document.querySelector("#nome");
     const nome = nomeInput.value;
-    
+  
     const options = {
-        margin: [15, 0, 15, 0],
-        filename: `${nome}.pdf`,
-        scale: 4,
-        image: { type: 'jpeg', quality: 1},
-        html2canvas: {
-            dpi: 300,
-            scale:4,
-            enableLinks:true,
-            DisablePdfCompression:1,
-            letterRendering: true,
-            useCORS: true
-          },
-        jsPDF:{unit:"pt", format:"a4", orientation:"portrait"}
+      filename: `${nome}.pdf`,
+      image: { type: 'jpeg', quality: 1 },
+      html2canvas: {
+        dpi: 300,
+        letterRendering: true,
+        useCORS: true,
+        scale: 6
+      },
+      jsPDF: { unit: "pt", format: [content.offsetWidth, content.offsetHeight], orientation: "portrait" }
     };
+  
     if (nome.trim() !== '') {
-        html2pdf().set(options).from(content).save();
+      html2pdf().set(options).from(content).save();
     } else {
-        alert("Por favor, insira um nome antes de gerar o certificado.");
+      alert("Por favor, insira um nome antes de gerar o certificado.");
     }
-});
+  });
